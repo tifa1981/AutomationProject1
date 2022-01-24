e475c211f67a933bef0aa8dfd6f514aa4fdc1475
YanchunZhou <tifa1981@gmail.com>
2022-01-24 14:05:02 -0500

    added more hello feature

--- src/gitPractice1/HelloTest.java
+++ src/gitPractice1/HelloTest.java
@@ -4,5 +4,7 @@ public class HelloTest {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("Developing hello feature");
+        System.out.println("Local repo is already published to Github");
+        System.out.println("Doing more work");
     }
 }
